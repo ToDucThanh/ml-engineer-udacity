66334de589468dfdfc00ffe65809e4cacd1f834c
ToDucThanh <toducthanh155@gmail.com>
2023-10-05 06:13:42 +0700

    Update inference.py

--- Project_3/src/inference.py
+++ Project_3/src/inference.py
@@ -7,7 +7,6 @@ from net import Net
 JSON_CONTENT_TYPE = "application/json"
 
 def model_fn(model_dir):
-    print("Loading model")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Net(device)
     with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
@@ -18,9 +17,7 @@ def model_fn(model_dir):
     return model
     
 def input_fn(request_body, request_content_type):
-    print("Input1")
     if request_content_type == "application/json":
-        print("Input2")
         input_data = json.loads(request_body)
         
         return input_data
@@ -28,19 +25,15 @@ def input_fn(request_body, request_content_type):
     else:
         raise Exception("Requested unsupported ContentType in Accept: " + content_type)
         
-        return
-
-def predict_fn(input_data, model):
-
-    print("Got input Data: {}".format(input_data))
+        return 
     
+def predict_fn(input_data, model):    
     with torch.no_grad():
         predictions = model(input_data)
     
     return predictions
 
 def output_fn(predictions, content_type):
-    print("PREDICTION: ", predictions)
     if content_type == "application/json":
         res = predictions.cpu().detach().numpy().tolist()
         
